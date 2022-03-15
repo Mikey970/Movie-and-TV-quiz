00beca56694fe2cbe37f089185ee053371bc4c30
Mike Delucia <mikey970@yahoo.com>
2022-03-15 10:50:51 -0400

    changes. Lines starting updationg project

--- page2.js
+++ page2.js
@@ -1,2 +1,8 @@
 let photoContainer = document.querySelector(".photo")
 photoContainer.src = "img/batfish.jpeg"
+let headerNumber = document.querySelector(".questionHeader") 
+let question = document.querySelector(".mainQuestion")
+let button1 = document.querySelector("#button1")
+let button2 = document.querySelector("#button2")
+let button3 = document.querySelector("#button3")
+let button4 = document.querySelector("#button4")
