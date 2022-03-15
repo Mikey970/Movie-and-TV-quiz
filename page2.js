0b4b2febc36ceb55d0de63ffa42b1ccb8d01e98a
Mike Delucia <mikey970@yahoo.com>
2022-03-15 11:48:23 -0400

    beginning 2nd question

--- page2.js
+++ page2.js
@@ -6,3 +6,16 @@ let button1 = document.querySelector("#button1")
 let button2 = document.querySelector("#button2")
 let button3 = document.querySelector("#button3")
 let button4 = document.querySelector("#button4")
+let score = 0;
+let currentQuestion = 1;
+button1.addEventListener("click", changePage)
+function changePage() {
+  if (currentQuestion == 1) {
+    headerNumber.innerHTML = "Question 2";
+    question.innerHTML = "This endangered species of Lemur is found in the rainforests of madagascar. Its know for knocking on trees to find the hollow points. Do you know it's name?"
+    photoContainer.src = "img/ayeaye.jpg"
+    button1.value = "superman"
+  }
+  currentQuestion++
+ 
+}
