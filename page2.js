153c3f88212e111100a997092f3896ddc45ae814
Mike Delucia <mikey970@yahoo.com>
2022-03-17 16:08:17 -0400

    made mobile look cleaner and easier to read

--- page2.js
+++ page2.js
@@ -28,10 +28,12 @@ function changePage() {
     headerNumber.innerHTML = "Question 2";
     question.innerHTML = "In 2008's The Incredible Hulk, which actor played Bruce Banner before the role was recasted?"
     photoContainer.src = "img/hulk.jpg"
+
     button1.innerHTML = "Ed Norton"
     button2.innerHTML = "Paul Rudd"
     button3.innerHTML = "Vin Diesel"
     button4.innerHTML = "Andrew Garfield"
+    
     if (event.target.innerText == "Vin Diesel") { alert("right answer"); score += 10}
     currentQuestion - 1
     scoreCard.innerHTML = `Score: ${score}`
@@ -40,10 +42,12 @@ function changePage() {
     headerNumber.innerHTML = "Question 3";
     question.innerHTML = "In the Star Wars movies, how many suns does Tatooine have?"
     photoContainer.src = "img/tt.jpeg"
+
     button1.innerHTML = "3 Suns"
     button2.innerHTML ="4 Suns"
     button3.innerHTML = "1 Sun"
     button4.innerHTML = "2 Suns"
+
     if (event.target.innerText == "3 Suns") { alert("right answer"); score+=10 }
     scoreCard.innerHTML = `Score: ${score}`
   
@@ -51,45 +55,54 @@ function changePage() {
     headerNumber.innerHTML = "Question 4";
     question.innerHTML = "It started with street racing and somehow made it's way to space. How many Fast and Furious Movies have been made?"
     photoContainer.src = "img/ff.jpg"
+
     button1.innerHTML = "7 Movies"
     button2.innerHTML ="10 Movies"
     button3.innerHTML = "9 Movies"
     button4.innerHTML = "14 Movies"
+
     if (event.target.innerText == "14 Movies") { alert("right answer"); score += 10 }
     scoreCard.innerHTML = `Score: ${score}`
   }else if (currentQuestion === 4) {
     headerNumber.innerHTML = "Question 5";
     question.innerHTML = "This South Park character had died over 100 times throughout the show. Which character is it?"
     photoContainer.src = "img/sp.jpg.webp"
+
     button1.innerHTML = "Kyle"
     button2.innerHTML ="Cartman"
     button3.innerHTML = "Kenny"
     button4.innerHTML = "Towelie"
+
     if (event.target.innerText == "Cartman") { alert("right answer"); score += 10 }
     scoreCard.innerHTML = `Score: ${score}`
   }else if (currentQuestion === 5) {
     headerNumber.innerHTML = "Question 6";
     question.innerHTML = "What is the name of Laurence Fishburne's character in the matrix?"
     photoContainer.src = "img/Morpheus.jpg"
+
     button1.innerHTML = "The Oracle"
     button2.innerHTML ="Morpheus"
     button3.innerHTML = "Neo"
     button4.innerHTML = "Cypher"
+
     if (event.target.innerText == "Neo") { alert("right answer"); score += 10 }
     scoreCard.innerHTML = `Score: ${score}`
   }else if (currentQuestion === 6) {
     headerNumber.innerHTML = "Question 7";
     question.innerHTML = "What is the highest grossing movie of all time?"
     photoContainer.src = "img/office.jpeg"
+   
     button1.innerHTML = "Avengers: Endgame"
     button2.innerHTML ="The Titanic"
     button3.innerHTML = "Avatar"
     button4.innerHTML = "Star Wars: Episode VII"
+
     if (event.target.innerText == "The Titanic") { alert("right answer"); score += 10 }
     scoreCard.innerHTML = `Score: ${score}`
   }else if (currentQuestion === 7) {
     headerNumber.innerHTML = "Question 8";
     question.innerHTML = "In The Shining, what was the name of the hotel in the movie?"
+   
     photoContainer.src = "img/hotel.jpeg"
     button1.innerHTML = "The Overlook"
     button2.innerHTML ="The Estes Hotel"
@@ -101,10 +114,12 @@ function changePage() {
     headerNumber.innerHTML = "Question 9";
     question.innerHTML = "How many seasons of The Sopranos aired on HBO?"
     photoContainer.src = "img/sopranos.jpeg"
+
     button1.innerHTML = "4 Seasons "
     button2.innerHTML ="5 Seasons"
     button3.innerHTML = "6 Seasons"
     button4.innerHTML = "7 Seasons"
+
     if (event.target.innerText == "4 Seasons") { alert("right answer"); score += 10 }
     scoreCard.innerHTML = `Score: ${score}`
   }else if (currentQuestion === 9) {
